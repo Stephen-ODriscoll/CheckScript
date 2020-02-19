48914c30ba6ab065933f82ea4e6e37f42a975fe6
Stephen O Driscoll <R00146853@student-cit.local>
2020-02-19 21:20:43 +0000

    More untested changes

--- checkScript.py
+++ checkScript.py
@@ -4,6 +4,7 @@ import subprocess
 from datetime import datetime
 from collections import defaultdict
 
+DETACHED_PROCESS = 0x00000008
 erase_after_run = True
 log = open("CheckLog.txt", 'a')
 defaultConfig = {"timeout": 30}
@@ -11,73 +12,36 @@ teams_folder = "C:\\Users\\steph\\OneDrive\\Desktop\\checkScript\\teams"
 tests_folder = "C:\\Users\\steph\\OneDrive\\Desktop\\checkScript\\tests"
 
 
-def p1(file):
-    inputs = [problems_folder + "\\P1\\input00.txt", problems_folder + "\\P1\\input01.txt",
-              problems_folder + "\\P1\\input02.txt", problems_folder + "\\P1\\input03.txt"]
-    outputs = [problems_folder + "\\P1\\output00.txt", problems_folder + "\\P1\\output01.txt",
-               problems_folder + "\\P1\\output02.txt", problems_folder + "\\P1\\output03.txt"]
-    check_problem(file, inputs, outputs, 30)
+def run_tests(file, configs, inputs, outputs):
 
+    # Validate input file integrity, if a file was modified it may have been modified maliciously
+    for input_file in inputs:
+        if input_file[1] != os.path.getmtime(input_file[0]):
+            write("FATAL: " + input_file[0] + " was modified, this action may have been malicious")
+            exit()
 
-def p2(file):
-    inputs = [problems_folder + "\\P2\\input00.txt", problems_folder + "\\P2\\input01.txt",
-              problems_folder + "\\P2\\input02.txt", problems_folder + "\\P2\\input03.txt",
-              problems_folder + "\\P2\\input04.txt"]
-    outputs = [problems_folder + "\\P2\\output00.txt", problems_folder + "\\P2\\output01.txt",
-               problems_folder + "\\P2\\output02.txt", problems_folder + "\\P2\\output03.txt",
-               problems_folder + "\\P2\\output04.txt"]
-    check_problem(file, inputs, outputs, 20)
-
-
-def p3(file):
-    inputs = [problems_folder + "\\P3\\input00.txt", problems_folder + "\\P3\\input01.txt",
-              problems_folder + "\\P3\\input02.txt", problems_folder + "\\P3\\input03.txt"]
-    outputs = [problems_folder + "\\P3\\output00.txt", problems_folder + "\\P3\\output01.txt",
-               problems_folder + "\\P3\\output02.txt", problems_folder + "\\P3\\output03.txt"]
-    check_problem(file, inputs, outputs, 10)
-
-
-def p4(file):
-    inputs = [problems_folder + "\\P4\\input00.txt", problems_folder + "\\P4\\input01.txt",
-              problems_folder + "\\P4\\input02.txt", problems_folder + "\\P4\\input03.txt",
-              problems_folder + "\\P4\\input04.txt"]
-    outputs = [problems_folder + "\\P4\\output00.txt", problems_folder + "\\P4\\output01.txt",
-               problems_folder + "\\P4\\output02.txt", problems_folder + "\\P4\\output03.txt",
-               problems_folder + "\\P4\\output04.txt"]
-    check_problem(file, inputs, outputs, 10)
-
-
-def p5(file):
-    inputs = [problems_folder + "\\P5\\input00.txt", problems_folder + "\\P5\\input01.txt",
-              problems_folder + "\\P5\\input02.txt", problems_folder + "\\P5\\input03.txt"]
-    outputs = [problems_folder + "\\P5\\output00.txt", problems_folder + "\\P5\\output01.txt",
-               problems_folder + "\\P5\\output02.txt", problems_folder + "\\P5\\output03.txt"]
-    check_problem(file, inputs, outputs, 10)
-
-
-
-def run_tests(file, inputs, outputs, timeout):
     try:
-        for i  in range(len(inputs)):
+        for i in range(len(inputs)):
             if file.endswith(".exe"):
-                command = file + " " + inputs[i] + " output.txt"
+                command = "\"" + file + "\" \"" + inputs[i][0] + "\" output.txt"
             elif file.endswith(".py"):
-                command = "python " + file + " " + inputs[i] + " output.txt"
+                command = "python \"" + file + "\" \"" + inputs[i][0] + "\" output.txt"
             elif file.endswith(".java"):
-                subprocess.call("javac " + file)
-                command = "java " + file[:len(file) - 5] + " " + inputs[i] + " output.txt"
+                subprocess.call("javac \"" + file + "\"")
+                command = "java \"" + file[:-5] + "\" \"" + inputs[i][0] + "\" output.txt"
             else:
-                break
+                write("File extension " + os.path.splitext(file)[1] + " not recognised")
+                return
 
             if os.path.exists("output.txt"):
                 os.remove("output.txt")
 
             write("Executing: " + command)
-            subprocess.Popen(command, shell=True, creationflags=subprocess.DETACHED_PROCESS).wait(timeout)
+            subprocess.Popen(command, shell=True, creationflags=DETACHED_PROCESS).wait(configs["timeout"])
 
             with open('output.txt') as output, open(outputs[i]) as expected:
                 if not filecmp.cmp(output, expected, False):
-                    write(file + " failed test case " + inputs[i] + "\n")
+                    write(file + " failed test case " + inputs[i][0] + "\n")
                     return
 
         write(file + " successfully passed all tests\n")
@@ -85,7 +49,7 @@ def run_tests(file, inputs, outputs, timeout):
     except subprocess.CalledProcessError:
         write(file + " threw an exception during execution\n")
     except subprocess.TimeoutExpired:
-        write(file + " was timed out after " + str(timeout) + " seconds\n")
+        write(file + " was timed out after " + str(configs["timeout"]) + " seconds\n")
     except FileNotFoundError:
         write("File not found error (Maybe output file wasn't created)\n")
     except Exception:
@@ -99,7 +63,7 @@ def parse_config_file(file_path):
     with open(file_path) as config_file:
         for line in config_file:
             splits = line.split('=')
-            config[splits[0]] = splits[1]
+            config[splits[0]] = int(splits[1])
 
     # Add default config for unspecified options
     for key, value in defaultConfig.items():
@@ -116,9 +80,9 @@ def main():
     for folder in os.listdir(tests_folder):
         found_config = False
         for file in os.path.join(tests_folder, folder):
-            file_path = os.path.join(tests_folder, folder, file)
+            file_path = os.path.join(tests_folder, folder, file).lower()
             if file_path.endswith(".in"):
-                file_path2 = file_path[:-3] + ".out"
+                file_path2 = (file_path[:-3] + ".out").lower()
                 if os.path.exists(file_path2):
                     tests[folder][1].append(tuple((file_path, os.path.getmtime(file_path))))
                     tests[folder][2].append(tuple((file_path2, os.path.getmtime(file_path2))))
@@ -137,13 +101,13 @@ def main():
         for folder in os.listdir(teams_folder):
             for file in os.listdir(os.path.join(teams_folder, folder)):
 
-                file_path = os.path.join(teams_folder, folder, file)
+                file_path = os.path.join(teams_folder, folder, file).lower()
                 if file_path in files_to_ignore:
                     continue
 
                 key = os.path.splitext(file)
                 if key in tests:
-                    run_tests(file_path, tests[key])
+                    run_tests(file_path, tests[key][0], tests[key][1], tests[key][2])
                 else:
                     write(file + " doesn't match any tests and will be ignored")
 
@@ -160,8 +124,7 @@ def main():
 
 def write(message):
     print(message)
-    now = datetime.now()
-    log.write(str(now) + "\t" + message)
+    log.write(str(datetime.now()) + "\t" + message)
     log.flush()
 
 
